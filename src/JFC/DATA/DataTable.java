2ab5a66b9d65f4b7e381aac52b6515c9110a0bde
forduna <46443076+forduna-iiasa@users.noreply.github.com>
2022-03-01 19:09:47 +0100

    adding objects folder

---
+++ src/JFC/DATA/DataTable.java
@@ -0,0 +1,13 @@
+package JFC.DATA;
+
+public class DataTable {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- src/JFC/IIASA/Main.java
+++ src/JFC/IIASA/Main.java
@@ -1,9 +1,13 @@
 package JFC.IIASA;
 
+import JFC.DATA.DataTable;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("si se peude dfg sdf");
+        DataTable tbl = new DataTable();
+        tbl.setId(3);
+        System.out.println("si se peude dfg sdf:" + tbl.getId());
 	// write your code here
     }
 }
