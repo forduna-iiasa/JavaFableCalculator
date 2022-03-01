0b3a152b7c05f108acc2bc33a8992870464f0470
forduna <46443076+forduna-iiasa@users.noreply.github.com>
2022-03-01 19:02:32 +0100

    Update Main.java

--- src/JFC/IIASA/Main.java
+++ src/JFC/IIASA/Main.java
@@ -3,7 +3,7 @@ package JFC.IIASA;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("si se peude");
+        System.out.println("si se peude dfg sdf");
 	// write your code here
     }
 }
