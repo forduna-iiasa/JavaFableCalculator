05a0260b3fc6763325828f1d7ba69550334ae03c
forduna <46443076+forduna-iiasa@users.noreply.github.com>
2022-03-17 13:03:49 +0100

    Getting full path of formulas used in a table

--- src/JFC/IIASA/Main.java
+++ src/JFC/IIASA/Main.java
@@ -5,9 +5,12 @@ import JFC.EQUATIONS.LoadEquation;
 import JFC.STRUCTS.NodeMetaCase;
 import JFC.STRUCTS.NodeTable;
 import JFC.STRUCTS.RowCols;
+import jade.domain.FIPANames;
+import javafx.scene.control.Tab;
 import org.apache.poi.openxml4j.util.ZipSecureFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
@@ -15,10 +18,12 @@ import java.util.Stack;
 public class Main {
     Stack<String> pila;
 
+
     public static void main(String[] args) throws IOException {
         Main m = new Main();
         Lex Lexico = new Lex();
         NodeTable McTables = new BuildMcTables().BuildTables();
+        m.pila = new Stack<String>();
         //NodeTable RetrievedTable;
         //asi recuperamos una tabla de la estructura
      //   RetrievedTable = McTables.retrieveTable(McTables,"Diet_scen");
@@ -26,7 +31,11 @@ public class Main {
         int a = McTables.getColLocation(McTables,"Reporting_aggregate","kcal_hist");
         String g = McTables.getCellValue(McTables,"Reporting_aggregate",2,a);
         Lexico.getTokens(g);
-        System.out.println("-Rowlocation-"+g);
+        m.getPath(Lexico,McTables);
+        m.printPila();
+
+
+
 
        /* for(int i=1;i<2;i++) {
             System.out.println("Row["+i+"]");
@@ -48,24 +57,36 @@ public class Main {
 
     }
 
-    public void getPath(Lex lexico) {
-        pila = new Stack<String>();
+    public void getPath(Lex lexico,NodeTable McTables) throws FileNotFoundException {
+        boolean gotTable =false;
+        String TableName="",ColName="";
+
         int tok=0;
         for(int i=0;i<lexico.Toks.size();i++){
             tok = (int) lexico.Toks.get(i);
-            if(tok == 300){
-                pila.push(lexico.Cads.get(i).toString());
+            if(tok == 300) {
+                TableName = lexico.Cads.get(i).toString();
+                gotTable = true;
             }
-
-        }
-
-
-
-        for(int h=0;h<pila.size();h++){
-            System.out.println(pila.peek());
-            pila.pop();
+            if(tok == -1 && gotTable == true){
+                gotTable=false;
+                ColName = lexico.Cads.get(i).toString();
+                pila.push(TableName+"."+ColName);
+                System.out.println(TableName+"."+ColName+"-->");
+                Lex Lexico = new Lex();
+                int a = McTables.getColLocation(McTables, TableName,ColName);
+                String ga = McTables.getCellValue(McTables,TableName,2,a);
+                Lexico.getTokens(ga);
+                getPath(Lexico,McTables);
+            }
         }
     }
 
+    public void printPila(){
+        while(!pila.empty()){
+            System.out.println(this.pila.peek());
+            pila.pop();
+        }
+    }
 
 }
